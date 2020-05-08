e157cbc0c12480213ffb7a0abb6ed34e4f8f89e1
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-08 09:52:34 -0700

    POST: create a new action

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -7,5 +7,24 @@ router.get("/", async (req, res, next) => {
   res.json(actions);
 });
 
-
+router.post("/", async (req, res, next) => {
+  try {
+    let data = {
+      project_id: req.body.project_id,
+      description: req.body.description,
+      notes: req.body.notes
+    };
+    if (!data.project_id ||
+         !data.description ||
+         !data.notes) {
+      return res.status(401).json({
+        message: "project_id, description & notes should be all filled in"
+      });
+    }
+    const newAction = await actionsDb.insert(data);
+    res.status(201).json(newAction);
+  } catch (err) {
+    next(err);
+  }
+});
 module.exports = router;
