96b3245cd3fa2e634e665b02bba27642e502e623
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-08 09:57:35 -0700

    GET: ':/id' get an action

--- actions/actions-router.js
+++ actions/actions-router.js
@@ -3,8 +3,12 @@ const actionsDb = require("../data/helpers/actionModel");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  const actions = await actionsDb.get();
-  res.json(actions);
+  try {
+    const actions = await actionsDb.get();
+    res.json(actions);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.post("/", async (req, res, next) => {
@@ -14,15 +18,25 @@ router.post("/", async (req, res, next) => {
       description: req.body.description,
       notes: req.body.notes
     };
-    if (!data.project_id ||
-         !data.description ||
-         !data.notes) {
+    if (!data.project_id || !data.description || !data.notes) {
       return res.status(401).json({
         message: "project_id, description & notes should be all filled in"
       });
     }
     const newAction = await actionsDb.insert(data);
     res.status(201).json(newAction);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const action = await actionsDb.get(req.params.id);
+    if (!action) {
+      return res.status(404).json({ message: "Action not found" });
+    }
+    res.status(201).json(action);
   } catch (err) {
     next(err);
   }
